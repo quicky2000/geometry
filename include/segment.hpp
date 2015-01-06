bffabab1841871408312f9be53f53728276f350e
Julien Thevenon <julien_thevenon@yahoo.fr>
2015-01-06 08:39:46 +0100

    add scalar_product, vectorial_product

--- include/segment.hpp
+++ include/segment.hpp
@@ -1,3 +1,22 @@
+/*
+    This file is part of geometry
+    Copyright (C) 2011  Julien Thevenon ( julien_thevenon at yahoo.fr )
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef _SEGMENT_HPP_
+#define _SEGMENT_HPP_
 
 #include "point.hpp"
 #include "assert.h"
@@ -10,9 +29,14 @@ public:
   inline const point & get_dest(void)const;
   inline bool is_horizontal(void)const;
   inline bool is_vertical(void)const;
-  inline double get_x(const double & p_y);
-  inline double get_y(const double & p_x);
-  inline bool belong(const point & p_point);
+  inline double get_x(const double & p_y)const;
+  inline double get_y(const double & p_x)const;
+  inline bool belong(const point & p_point)const;
+  inline double get_side(const point & p_point)const;
+  inline double vectorial_product(const segment & p_seg)const;
+  inline double scalar_product(const segment & p_seg)const;
+  inline double get_square_size(void)const;
+  inline static bool check_convex_continuation(const double & p_vec_prod,double & p_orient);
 private:
   point m_source;
   point m_dest;
@@ -60,7 +84,7 @@ bool segment::is_vertical(void)const
   return m_vertical;
 }
 
-double segment::get_x(const double & p_y)
+double segment::get_x(const double & p_y)const
 {
   assert(!m_horizontal);
   double l_x = m_source.get_x();
@@ -72,7 +96,7 @@ double segment::get_x(const double & p_y)
   return l_x;
 }
 
-double segment::get_y(const double & p_x)
+double segment::get_y(const double & p_x)const
 {
   assert(!m_vertical);
   double l_y = m_source.get_y();
@@ -84,7 +108,47 @@ double segment::get_y(const double & p_x)
   return l_y;
 }
 
-bool segment::belong(const point & p_point)
+bool segment::belong(const point & p_point)const
 {
   return p_point.get_y() == this->get_y(p_point.get_x()) && p_point.get_x() >= m_min_x && p_point.get_x() <= m_max_x && p_point.get_y() >= m_min_y && p_point.get_y() <= m_max_y;
 }
+
+double segment::get_side(const point & p_point)const
+{
+  return m_coef_x * (p_point.get_y() - m_source.get_y()) - m_coef_y * (p_point.get_x() - m_source.get_x());
+}
+
+double segment::vectorial_product(const segment & p_seg)const
+{
+  return m_coef_x * p_seg.m_coef_y - m_coef_y * p_seg.m_coef_x ;
+}
+
+double segment::scalar_product(const segment & p_seg)const
+{
+  return m_coef_x * p_seg.m_coef_x + m_coef_y * p_seg.m_coef_y ;
+}
+
+double segment::get_square_size(void)const
+{
+  return m_coef_x * m_coef_x+ m_coef_y * m_coef_y;
+}
+
+bool segment::check_convex_continuation(const double & p_vec_prod,double & p_orient)
+{
+  bool l_convex = true;
+  if(p_orient < 0)
+    {
+      l_convex = p_vec_prod <= 0;
+    }
+  else if(p_orient > 0)
+    {
+      l_convex = p_vec_prod >= 0;
+    }
+  else
+    {
+      p_orient = p_vec_prod;
+    }
+  return l_convex;
+}
+
+#endif /* _SEGMENT_HPP_ */
