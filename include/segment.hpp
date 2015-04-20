fd1fc025790dc288418cb1ecd02cb8b138019f84
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-04-20 16:55:59 +0200

    Improve intersec code, add new constructor, add enum to define segment orientation: _ Improve intersec code to have a "light" that just return a boolean and a more complex method returning intersection poit if it exits _ Add a constructor with only source and destination coordinates _ Add an enumerated type to definie is segment is horizontal vertical or something else

--- include/segment.hpp
+++ include/segment.hpp
@@ -34,6 +34,7 @@ namespace geometry
   {
     friend  std::ostream & operator<< <>(std::ostream & p_stream, const segment<T> & p_segment);
   public:
+    inline segment(const T & p_source_x,const T & p_source_y,const T & p_dest_x,const T & p_dest_y);
     inline segment(const point<T> & p_source, const point<T> & dest);
     inline const point<T> & get_source(void)const;
     inline const point<T> & get_dest(void)const;
@@ -50,12 +51,16 @@ namespace geometry
     inline const T & get_max_x(void)const;
     inline const T & get_min_y(void)const;
     inline const T & get_max_y(void)const;
+    inline bool intersec(const segment<T> & p_seg)const;
+    inline bool intersec(const segment<T> & p_seg,bool & p_single_point,point<T> & p_intersec)const;
     inline static bool check_convex_continuation(const T & p_vec_prod,T & p_orient, bool p_init);
   private:
+    typedef enum class segment_orient {OTHER=0,HORIZONTAL,VERTICAL} t_segment_orient;
     point<T> m_source;
     point<T> m_dest;
     T m_coef_x;
     T m_coef_y;
+    t_segment_orient m_orient;
     bool m_horizontal;
     bool m_vertical;
     T m_min_x;
@@ -78,6 +83,7 @@ namespace geometry
     m_dest(p_dest),
     m_coef_x(p_dest.get_x() - p_source.get_x()),
     m_coef_y(p_dest.get_y() - p_source.get_y()),
+    m_orient(m_coef_x==0 ? t_segment_orient::VERTICAL : ( m_coef_y==0 ? t_segment_orient::HORIZONTAL : t_segment_orient::OTHER)),
     m_horizontal(m_coef_y==0),
     m_vertical(m_coef_x==0),
     m_min_x( p_source.get_x() <= p_dest.get_x() ? p_source.get_x() : p_dest.get_x()),
@@ -86,6 +92,14 @@ namespace geometry
     m_max_y( p_source.get_y() >= p_dest.get_y() ? p_source.get_y() : p_dest.get_y())
   {
   }
+
+  //----------------------------------------------------------------------------
+  template <typename T> 
+  segment<T>::segment(const T & p_source_x,const T & p_source_y,const T & p_dest_x,const T & p_dest_y):
+    segment(point<T>(p_source_x,p_source_y),point<T>(p_dest_x,p_dest_y))
+  {
+  }
+
 
   //----------------------------------------------------------------------------
   template <typename T> 
@@ -188,6 +202,256 @@ namespace geometry
   T segment<T>::get_square_size(void)const
   {
     return m_coef_x * m_coef_x+ m_coef_y * m_coef_y;
+  }
+
+  //----------------------------------------------------------------------------
+  template <typename T> 
+  bool segment<T>::intersec(const segment<T> & p_seg)const
+  {
+    // Check if some extremities are common
+    //    if(m_source == p_seg.get_source() || m_source == p_seg.get_dest() || m_dest == p_seg.get_source() || m_dest == p_seg.get_dest())
+    //      {
+    //        return true;
+    //      }
+    if(!this->vectorial_product(p_seg))
+      {
+        switch(m_orient)
+          {
+          case t_segment_orient::OTHER:
+            if(get_x(0) == p_seg.get_x(0))
+              {
+                return ((p_seg.m_min_x <= m_min_x && m_min_x <= p_seg.m_max_x) || 
+                        (p_seg.m_min_x <= m_max_x && m_max_x <= p_seg.m_max_x) ||
+                        (m_min_x <= p_seg.m_max_x && p_seg.m_max_x <= m_max_x) ||
+                        (m_min_x <= p_seg.m_min_x && p_seg.m_min_x <= m_max_x)); 
+              }
+            else
+              {
+                return false;
+              }
+            break;
+          case t_segment_orient::HORIZONTAL:
+            if(m_source.get_y() == p_seg.m_source.get_y())
+              {
+                return ((p_seg.m_min_x <= m_min_x && m_min_x <= p_seg.m_max_x) || 
+                        (p_seg.m_min_x <= m_max_x && m_max_x <= p_seg.m_max_x) ||
+                        (m_min_x <= p_seg.m_max_x && p_seg.m_max_x <= m_max_x) ||
+                        (m_min_x <= p_seg.m_min_x && p_seg.m_min_x <= m_max_x)) ; 
+                break;
+              }
+            return false;
+          case t_segment_orient::VERTICAL:
+            if(m_source.get_x() == p_seg.m_source.get_x())
+              {
+                return ((p_seg.m_min_y <= m_min_y && m_min_y <= p_seg.m_max_y) || 
+                        (p_seg.m_min_y <= m_max_y && m_max_y <= p_seg.m_max_y) ||
+                        (m_min_y <= p_seg.m_max_y && p_seg.m_max_y <= m_max_y) ||
+                        (m_min_y <= p_seg.m_min_y && p_seg.m_min_y <= m_max_y)) ; 
+              }
+            return false;
+            break;
+          }
+      }
+    if(this->vectorial_product(segment<T>(m_source,p_seg.get_source())) * this->vectorial_product(segment<T>(m_source,p_seg.get_dest())) < 0 && p_seg.vectorial_product(segment<T>(p_seg.m_source,m_source)) * p_seg.vectorial_product(segment<T>(p_seg.m_source,m_dest)) < 0)
+      {
+        return true;
+      }
+    return false;
+  }
+
+  //----------------------------------------------------------------------------
+  template <typename T> 
+  bool segment<T>::intersec(const segment<T> & p_seg,bool & p_single_point,point<T> & p_intersec)const
+  {
+    //    if(m_source == p_seg.get_source() || m_source == p_seg.get_dest())
+    //      {
+    //        p_single_point = true;
+    //        p_intersec = m_source;
+    //        return true;
+    //      }
+    //    if(m_dest == p_seg.get_source() || m_dest == p_seg.get_dest())
+    //      {
+    //        p_single_point = true;
+    //        p_intersec = m_dest;
+    //        return true;
+    //      }
+
+    switch(((int)m_orient) * 3 + ((int)p_seg.m_orient))
+      {
+      case ((int)t_segment_orient::OTHER) * 3 + ((int)t_segment_orient::OTHER):
+        {
+          if(!this->vectorial_product(p_seg))
+            {
+              p_single_point = false;
+              if(get_x(0) == p_seg.get_x(0))
+                {
+                  return ((p_seg.m_min_x <= m_min_x && m_min_x <= p_seg.m_max_x) || 
+                          (p_seg.m_min_x <= m_max_x && m_max_x <= p_seg.m_max_x) ||
+                          (m_min_x <= p_seg.m_max_x && p_seg.m_max_x <= m_max_x) ||
+                          (m_min_x <= p_seg.m_min_x && p_seg.m_min_x <= m_max_x)); 
+                }
+              else
+                {
+                  return false;
+                }
+            }
+          else if(this->vectorial_product(segment<T>(m_source,p_seg.get_source())) * this->vectorial_product(segment<T>(m_source,p_seg.get_dest())) < 0 && p_seg.vectorial_product(segment<T>(p_seg.m_source,m_source)) * p_seg.vectorial_product(segment<T>(p_seg.m_source,m_dest)) < 0)
+            {
+              p_single_point = true;
+              int l_case = 2 * (m_coef_x != p_seg.m_coef_x) + m_coef_y != p_seg.m_coef_y;
+              switch(l_case)
+                {
+                case 0:
+                  {
+                    T l_x = ((m_dest.get_x()  * p_seg.m_source.get_x() - m_source.get_x() * p_seg.m_dest.get_x()) / (m_coef_x - p_seg.m_coef_x));
+                    T l_y = ((m_dest.get_y()  * p_seg.m_source.get_y() - m_source.get_y() * p_seg.m_dest.get_y()) / (m_coef_y - p_seg.m_coef_y));
+                    p_intersec = point<T>(l_x,l_y);
+                  }
+                  break;
+                case 1:
+                  {
+                    T l_y = ((m_dest.get_y()  * p_seg.m_source.get_y() - m_source.get_y() * p_seg.m_dest.get_y()) / (m_coef_y - p_seg.m_coef_y));
+                    p_intersec = point<T>(get_x(l_y),l_y);                  
+                  }
+                  break;
+                case 2:
+                  {
+                    T l_x = ((m_dest.get_x()  * p_seg.m_source.get_x() - m_source.get_x() * p_seg.m_dest.get_x()) / (m_coef_x - p_seg.m_coef_x));
+                    p_intersec = point<T>(l_x,get_y(l_x));                  
+                  }
+                  break;
+                case 3:
+                  // Should not be possible as vectorial product is not null
+                  assert(false);
+                  break;
+                }
+              
+              return true;
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::OTHER) * 3 + ((int)t_segment_orient::HORIZONTAL):
+        {
+          if(m_min_y <= p_seg.m_source.get_y() && p_seg.m_source.get_y() <= m_max_y && p_seg.m_min_x <= this->get_x(p_seg.m_source.get_y()) && this->get_x(p_seg.m_source.get_y()) << p_seg.m_max_x)
+            {
+              p_single_point = true;
+              p_intersec = point<T>(this->get_x(p_seg.m_source.get_y()),p_seg.m_source.get_y());
+              return true;
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::OTHER) * 3 + ((int)t_segment_orient::VERTICAL):
+        {
+          T l_common_y = this->get_y(p_seg.m_source.get_x());
+          if(m_min_x <= p_seg.m_source.get_x() && p_seg.m_source.get_x() <= m_max_x &&  p_seg.m_min_y <= l_common_y && l_common_y  <= p_seg.m_max_y)
+            {
+              p_single_point = true;
+              p_intersec = point<T>(p_seg.m_source.get_x(),l_common_y);
+              return true;
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::HORIZONTAL) * 3 + ((int)t_segment_orient::OTHER):
+      case ((int)t_segment_orient::HORIZONTAL) * 3 + ((int)t_segment_orient::VERTICAL):
+        {
+          if(p_seg.get_min_y() <= m_source.get_y() && m_source.get_y() <= p_seg.get_max_y() && m_min_x <= p_seg.get_x(m_source.get_y()) && p_seg.get_x(m_source.get_y()) <= m_max_x)
+            {
+              p_single_point = true;
+              p_intersec = point<T>(p_seg.get_x(m_source.get_y()),m_source.get_y());
+              return true;
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::HORIZONTAL) * 3 + ((int)t_segment_orient::HORIZONTAL):
+        {
+          if(m_source.get_y() == p_seg.m_source.get_y())
+            {
+              if(m_max_x == p_seg.m_min_x)
+                {
+                  p_single_point = true;
+                  p_intersec = point<T>(m_max_x,m_source.get_y());
+                  return true;
+                }
+              if(m_min_x == p_seg.m_max_x)
+                {
+                  p_single_point = true;
+                  p_intersec = point<T>(m_min_x,m_source.get_y());
+                  return true;
+                }
+              p_single_point = false;
+              return (p_seg.m_min_x <= m_min_x && m_min_x <= p_seg.m_max_x) || 
+                (p_seg.m_min_x <= m_max_x && m_max_x <= p_seg.m_max_x) ||
+                (m_min_x <= p_seg.m_max_x && p_seg.m_max_x <= m_max_x) ||
+                (m_min_x <= p_seg.m_min_x && p_seg.m_min_x <= m_max_x) ; 
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::VERTICAL) * 3 + ((int)t_segment_orient::OTHER):
+      case ((int)t_segment_orient::VERTICAL) * 3 + ((int)t_segment_orient::HORIZONTAL):
+        {
+          if(p_seg.m_min_x <= m_source.get_x() && m_source.get_x() <= p_seg.m_max_x && m_min_y <= p_seg.get_y(m_source.get_x()) &&p_seg.get_y(m_source.get_x())  <= m_max_y)
+            {
+              p_single_point = true;
+              p_intersec = point<T>(m_source.get_x(),p_seg.get_y(m_source.get_x()));
+              return true;
+            }
+          else
+            {
+              return false;
+            }
+        }
+        break;
+      case ((int)t_segment_orient::VERTICAL) * 3 + ((int)t_segment_orient::VERTICAL):
+        {
+          if(m_source.get_x() == p_seg.m_source.get_x())
+            {
+              if(m_max_y == p_seg.m_min_y)
+                {
+                  p_single_point = true;
+                  p_intersec = point<T>(m_source.get_x(),m_max_y);
+                  return true;
+                }
+              if(m_min_y == p_seg.m_max_y)
+                {
+                  p_single_point = true;
+                  p_intersec = point<T>(m_source.get_x(),m_min_y);
+                  return true;
+                }
+              p_single_point = false;
+              return (p_seg.m_min_y <= m_min_y && m_min_y <= p_seg.m_max_y) || 
+                (p_seg.m_min_y <= m_max_y && m_max_y <= p_seg.m_max_y) ||
+                (m_min_y <= p_seg.m_max_y && p_seg.m_max_y <= m_max_y) ||
+                (m_min_y <= p_seg.m_min_y && p_seg.m_min_y <= m_max_y) ; 
+            }
+          else
+            {
+              return false;
+            }
+        }
+      }
+
+    p_single_point = false;
+    return false;
   }
 
   //----------------------------------------------------------------------------
